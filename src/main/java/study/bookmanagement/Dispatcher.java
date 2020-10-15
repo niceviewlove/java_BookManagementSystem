111919458c947f1ea4dc11b52749adc34db05ccb
Sion <Sion@DESKTOP-H79A2JN>
2020-10-15 18:25:02 +0900

    ControllerFactory 생성 후 Map으로 Dispatch 메소드 내부에 복잡한 if/else 정리

--- src/main/java/study/bookmanagement/Dispatcher.java
+++ src/main/java/study/bookmanagement/Dispatcher.java
@@ -1,55 +1,60 @@
 package study.bookmanagement;
 
-import java.util.Map;
+import study.bookmanagement.controller.Controller;
+import study.bookmanagement.controller.ControllerFactory;
 
-import study.bookmanagement.controller.BookCreateController;
-import study.bookmanagement.controller.BookInfoController;
-import study.bookmanagement.controller.BookListController;
-import study.bookmanagement.controller.BookRemoveController;
-import study.bookmanagement.controller.BookUpdateController;
-import study.bookmanagement.controller.MemberCreateController;
-import study.bookmanagement.controller.MemberListController;
-import study.bookmanagement.controller.MemberRemoveController;
-import study.bookmanagement.controller.MemberUpdateController;
 
 public class Dispatcher {
+	ControllerFactory controllerFactory;
 	
-	public HttpResponse<?> dispatch(HttpRequest httpRequest) {
-		// request에 있는 정보를 바탕으로 controller 실행하기
-		// url과 method 패턴에 맞는 controller를 찾아서 thread 생성
-		
-		//book..?
-		if (httpRequest.getUrl().equals("/books/create") && httpRequest.getMethod().equals("POST")) {
-			BookCreateController controller = new BookCreateController();
-			return controller.command(httpRequest);
-		} else if (httpRequest.getUrl().equals("/books/update") && httpRequest.getMethod().equals("PUT")) {
-			BookUpdateController controller = new BookUpdateController();
-			return controller.command(httpRequest);
-		} else if(httpRequest.getUrl().equals("/books/remove") && httpRequest.getMethod().equals("DELETE")) {
-			BookRemoveController controller = new BookRemoveController();
-			return controller.command(httpRequest);
-		} else if(httpRequest.getUrl().equals("/books") && httpRequest.getMethod().equals("GET")) {
-			BookListController controller = new BookListController();
-			return controller.command(httpRequest);
-		} else if(httpRequest.getUrl().equals("/book") && httpRequest.getMethod().equals("GET")) {
-			BookInfoController controller = new BookInfoController();
-			return controller.command(httpRequest);
-		}
-		
-		if (httpRequest.getUrl().equals("/members/create") && httpRequest.getMethod().equals("POST")) {
-			MemberCreateController controller = new MemberCreateController();
-			return controller.command(httpRequest);
-		} else if (httpRequest.getUrl().equals("/members/update/아이디값") && httpRequest.getMethod().equals("PUT")) {
-			MemberUpdateController controller = new MemberUpdateController();
-			return controller.command(httpRequest);
-		} else if(httpRequest.getUrl().equals("/members/remove/아이디값") && httpRequest.getMethod().equals("DELETE")) {
-			MemberRemoveController controller = new MemberRemoveController();
-			return controller.command(httpRequest);
-		} else if(httpRequest.getUrl().equals("/members") && httpRequest.getMethod().equals("GET")) {
-			MemberListController controller = new MemberListController();
-			return controller.command(httpRequest);
-		}
-		
-		throw new RuntimeException();
+	public Dispatcher() {
+		controllerFactory = new ControllerFactory();
 	}
+	
+	public static void main(String[] args) {
+		HttpRequest htttpRequest = new HttpRequest("/books/create", "POST", null, null);
+		Dispatcher dispatcher = new Dispatcher();
+		dispatcher.dispatch(htttpRequest);
+		dispatcher.dispatch(htttpRequest);
+	}
+
+	public HttpResponse<?> dispatch(HttpRequest httpRequest) {
+		Controller controller = controllerFactory.getInstance(ControllerFactory.getKey(httpRequest));
+		return controller.command(httpRequest);
+//		
+//		if (httpRequest.getUrl().equals("/books/create") && httpRequest.getMethod().equals("POST")) {
+//			BookCreateController controller = new BookCreateController();
+//			System.out.println("controller: "+controller);
+//			return controller.command(httpRequest);
+//		} else if (httpRequest.getUrl().equals("/books/update") && httpRequest.getMethod().equals("PUT")) {
+//			BookUpdateController controller = new BookUpdateController();
+//			return controller.command(httpRequest);
+//		} else if(httpRequest.getUrl().equals("/books/remove") && httpRequest.getMethod().equals("DELETE")) {
+//			BookRemoveController controller = new BookRemoveController();
+//			return controller.command(httpRequest);
+//		} else if(httpRequest.getUrl().equals("/books/list") && httpRequest.getMethod().equals("GET")) {
+//			BookListController controller = new BookListController();
+//			return controller.command(httpRequest);
+//		} else if(httpRequest.getUrl().equals("/book/info") && httpRequest.getMethod().equals("GET")) {
+//			BookInfoController controller = new BookInfoController();
+//			return controller.command(httpRequest);
+//		}
+//		
+//		if (httpRequest.getUrl().equals("/members/create") && httpRequest.getMethod().equals("POST")) {
+//			MemberCreateController controller = new MemberCreateController();
+//			return controller.command(httpRequest);
+//		} else if (httpRequest.getUrl().equals("/members/update") && httpRequest.getMethod().equals("PUT")) {
+//			MemberUpdateController controller = new MemberUpdateController();
+//			return controller.command(httpRequest);
+//		} else if(httpRequest.getUrl().equals("/members/remove") && httpRequest.getMethod().equals("DELETE")) {
+//			MemberRemoveController controller = new MemberRemoveController();
+//			return controller.command(httpRequest);
+//		} else if(httpRequest.getUrl().equals("/members") && httpRequest.getMethod().equals("GET")) {
+//			MemberListController controller = new MemberListController();
+//			return controller.command(httpRequest);
+//		}
+//		
+//		return null;
+	}
+
 }
---
+++ src/main/java/study/bookmanagement/controller/ControllerFactory.java
@@ -0,0 +1,38 @@
+package study.bookmanagement.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import study.bookmanagement.HttpRequest;
+
+public class ControllerFactory {
+	Map<String, Controller> controllers = new HashMap<>();
+	
+	public ControllerFactory() {
+		controllers.put("/books/create&POST", new BookCreateController());
+		controllers.put("/books/update&PUT", new BookUpdateController());
+		controllers.put("/books/remove&DELETE", new BookRemoveController());
+		controllers.put("/books/list&GET", new BookListController());
+		controllers.put("/books/info&GET", new BookInfoController());
+		
+		controllers.put("/members/create&POST", new MemberCreateController());
+		controllers.put("/members/update&PUT", new MemberUpdateController());
+		controllers.put("/members/remove&DELETE", new MemberRemoveController());
+		controllers.put("/members/list&GET", new MemberListController());
+	}
+	
+	public Controller getInstance(String key) {
+		Controller controller = controllers.get(key);
+		
+		if (controller == null) {
+			throw new RuntimeException("해당하는 " + key +" controller가 없습니다.");
+		}
+		
+		return controllers.get(key);
+	}
+	
+
+	public static String getKey(HttpRequest httpRequest) {
+		return httpRequest.getUrl() + "&" + httpRequest.getMethod();
+	}
+}
